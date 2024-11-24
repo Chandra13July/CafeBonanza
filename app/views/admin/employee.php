bf016320b5a9c7fe45e80fc609193cc414840d67
Chandra13July <chandra13july2004@gmail.com>
2024-11-24 21:26:37 +0700

    Update Data Tables

--- app/views/admin/employee.php
+++ app/views/admin/employee.php
@@ -216,7 +216,10 @@
         </div>
     </div>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="//cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
     <script>
+        let table = new DataTable('table');
         const deleteModal = document.getElementById('deleteModal');
         const addModal = document.getElementById('addModal');
         const editModal = document.getElementById('editModal');
